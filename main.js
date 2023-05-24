1170987923b08614f100b4c128d8c999d75e40c9
rimasumdev <67681547+rimasumdev@users.noreply.github.com>
2023-05-24 13:29:23 +0600

    JS Module

---
+++ main.js
@@ -0,0 +1,63 @@
+// Console Output
+console.log('This JavaScript Console Output');
+
+// Variables and Data Types
+let stringData = 'This is Masum Reza';
+let numberData = 5;
+let flotingData = 5.5;
+let booleanData = true;
+const arrayData = [1, 2, 3, 4, 5];
+
+// If Else Condition
+let a = 20;
+let b = 30;
+if (a > b) {
+  console.log(a + ' is greater than ' + b);
+} else if (a < b) {
+  console.log(b + ' is greater than ' + a);
+} else {
+  console.log('Both are Equal');
+}
+
+// For Loop
+let sum = 0;
+for (i = 0; i <= arrayData.length; i++) {
+  sum += i;
+}
+console.log('Sum Using For Loop: ', sum);
+
+// While Loop
+let j = 0;
+let sum2 = 0;
+while (j <= arrayData.length) {
+  sum2 += j;
+  j++;
+}
+console.log('Sum Using While Loop: ', sum2);
+
+// Do While Loop
+let k = 0;
+let sum3 = 0;
+do {
+  sum3 += k;
+  k++;
+} while (k <= arrayData.length);
+console.log('Sum Using Do While Loop: ', sum3);
+
+// Function
+function calculator(a, b) {
+  console.log(a + b);
+}
+calculator(10, 20);
+
+// Object
+const objData = {
+  name: 'Masum Reza',
+  age: 25,
+  district: 'Narail',
+  mobile: '01720446526',
+};
+console.log(objData.name);
+console.log(objData.age);
+console.log(objData.district);
+console.log(objData.mobile);
